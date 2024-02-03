a45d5b368d3c3b3d57134ee5e0e148d57e0e1b96
Charlie Sun <cssun@cs.cmu.edu>
2024-02-03 13:48:47 -0500

    add diff languages, return list of vids

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 from flask_cors import CORS
@@ -16,21 +16,38 @@ class Video(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     filename = db.Column(db.String(120), nullable=False)
+    language = db.Column(db.String(20), nullable=False)
 
 with app.app_context():
     db.create_all()
 
 @app.route('/upload', methods=['POST'])
 def upload():
+    language='en'
     if 'video' in request.files:
         video = request.files['video']
         filename = secure_filename(video.filename)
-        video.save(os.path.join(app.config['UPLOADED_VIDEOS_DEST'], filename))
-        new_video = Video(name=filename, filename=filename)
+        upload_path = os.path.join(app.config['UPLOADED_VIDEOS_DEST'], language, filename)
+        os.makedirs(os.path.dirname(upload_path), exist_ok=True)
+        video.save(upload_path)
+        new_video = Video(name=filename, filename=filename, language=language)
         db.session.add(new_video)
         db.session.commit()
         return 'Video uploaded and saved in database'
     return 'No video uploaded'
+
+@app.route('/videos', methods=['GET'])
+def get_videos():
+    videos = Video.query.all()
+    video_list = []
+    for video in videos:
+        video_list.append({
+            'id': video.id,
+            'name': video.name,
+            'filename': video.filename
+        })
+    return jsonify(video_list)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
